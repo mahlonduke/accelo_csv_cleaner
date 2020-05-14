b8fde42f580f6f1d3f11547dc94fe68abac580ab
mahlonduke <53539373+mahlonduke@users.noreply.github.com>
2020-05-14 11:04:58 -0700

    Updated requirements.txt to remove standard packages

---
+++ install_dependencies.py
@@ -0,0 +1,81 @@
+# Installs the necessary dependencies
+
+# Pandas
+try:
+    print ('Installing Pandas')
+    pip install pandas
+    print ('Pandas installation complete')
+    print ('------------------------------------------')
+    print (f'\n')
+except ImportError:
+   print ('Error installing Pandas')
+
+# NumPy
+try:
+    print ('Installing NumPy')
+    pip install numpy
+    print ('NumPy installation complete')
+    print ('------------------------------------------')
+    print (f'\n')
+except ImportError:
+   print ('Error installing NumPy')
+
+# Requests
+try:
+    print ('Installing Requests')
+    pip install requests
+    print ('Requests installation complete')
+    print ('------------------------------------------')
+    print (f'\n')
+except ImportError:
+   print 'Error installing Requests'
+
+# JSON
+try:
+    print ('Installing JSON')
+    ip install json
+    print ('JSON installation complete')
+    print ('------------------------------------------')
+    print (f'\n')
+except ImportError:
+   print ('Error installing JSON')
+
+# Pretty Print
+try:
+    print ('Installing Pretty Print')
+    pip install pprint
+    print ('Pretty Print installation complete')
+    print ('------------------------------------------')
+    print (f'\n')
+except ImportError:
+   print ('Error installing Pretty Print')
+
+# Math
+try:
+    print ('Installing Math')
+    pip install math
+    print ('Math installation complete')
+    print ('------------------------------------------')
+    print (f'\n')
+except ImportError:
+   print ('Error installing Math')
+
+# RE
+try:
+    print ('Installing RE')
+    pip install re
+    print ('RE installation complete')
+    print ('------------------------------------------')
+    print (f'\n')
+except ImportError:
+   print ('Error installing RE')
+
+# Base64
+try:
+    print ('Installing Base64')
+    pip install base64
+    print ('Base64 installation complete')
+    print ('------------------------------------------')
+    print (f'\n')
+except ImportError:
+   print ('Error installing Base64')
